2b2fc18111c768c55ea297fc1f94b3504211fc0c
Ale96Pa <palma.1871556@studenti.uniroma1.it>
2024-03-05 21:31:39 +0100

    demo

--- test-demos/test_get.py
+++ test-demos/test_get.py
@@ -2,12 +2,9 @@ import requests
 
 headers = {
     'Accept': 'application/ld+json',
+    'Link': '<https://raw.githubusercontent.com/Ale96Pa/data-model-security/main/DeviceInventory/context.json>; type="application/ld+json"',
 }
 
-params = {
-    'type': 'GtfsAgency',
-}
+response = requests.get('http://localhost:1026/ngsi-ld/v1/entities/?attrs=class', headers=headers)
 
-response = requests.get('http://localhost:1026/ngsi-ld/v1/entities', params=params, headers=headers)
-
-print(response.content)
+print(response.text)
---
+++ test-demos/test_post.py
@@ -0,0 +1,11 @@
+import requests
+
+headers = {
+    'Content-Type': 'application/ld+json',
+}
+
+data = '{    "id": "urn:test-device-2",    "type": "NetworkDevice",    "class": {        "type": "Property",        "value": "workstation"    },    "hostname": {        "type": "Property",        "value": "host1"    },    "HasInterfaces": {        "type": "Relationship",        "object": [            "urn:ngsi-ld:Floor:Test:SmartCitiesdomain:SmartBuildings:38vC2rMpPDpQ1cy52XqxrF",            "urn:ngsi-ld:Floor:Test:SmartCitiesdomain:SmartBuildings:0N3USWemr0KgB4UDlFuw2z",            "urn:ngsi-ld:Floor:Test:SmartCitiesdomain:SmartBuildings:0Jy7nvJPr4avJ7rEk394Tm",            "urn:ngsi-ld:Floor:Test:SmartCitiesdomain:SmartBuildings:1snjtpHWLBnQX6I36Y6QQU"        ]    },    "@context": ["https://raw.githubusercontent.com/Ale96Pa/data-model-security/main/DeviceInventory/context.json"]}'
+
+response = requests.post('http://localhost:1026/ngsi-ld/v1/entities', headers=headers, data=data)
+
+print(response.text)
